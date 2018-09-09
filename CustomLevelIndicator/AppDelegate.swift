83f313d0e2090734a718c01859e98aa4b2717b52
Douglas Adams <douglas.adams@me.com>
2018-09-08 21:00:51 -0400

    Initial Commit

---
+++ CustomLevelIndicator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomLevelIndicator
+//
+//  Created by Douglas Adams on 9/8/18.
+//  Copyright © 2018 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
